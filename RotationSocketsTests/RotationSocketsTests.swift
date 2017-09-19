5370cbde640e4e465dc171446f8698ec2c05c785
Jordy Sipkema <jordysipkema@gmail.com>
2017-09-19 15:09:50 +0200

    Initial Commit

---
+++ RotationSocketsTests/RotationSocketsTests.swift
@@ -0,0 +1,36 @@
+//
+//  RotationSocketsTests.swift
+//  RotationSocketsTests
+//
+//  Created by Jordy Sipkema on 19/09/2017.
+//  Copyright © 2017 Revcode. All rights reserved.
+//
+
+import XCTest
+@testable import RotationSockets
+
+class RotationSocketsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
